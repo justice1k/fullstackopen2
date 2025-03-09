d67373789ae096d433aa97fb95c23814b2e8a612
justice1k <justiceakpalu11@gmail.com>
2025-03-09 23:13:01 +0000

    cors

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.use(express.json())
+
+
+morgan.token('body', function (req) {
+  if (req.method === 'POST' && req.body) {
+    return JSON.stringify(req.body);
+  }
+  return '-';
+});
+
+
+app.use(morgan(':method :url :status :response-time ms - :body'));
 
 let persons = [
     { 
