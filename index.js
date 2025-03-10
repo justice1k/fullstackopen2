c26b46870fa77d0d1b4f4ef70ffe054d78a9397e
justice1k <justiceakpalu11@gmail.com>
2025-03-10 15:49:45 +0000

    env

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.get('/info', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}`)
